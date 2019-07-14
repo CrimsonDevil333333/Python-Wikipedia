9d4a66b787ce029e67a9a916ccc4ab500cfa6c70
Satyaa Goyal <49252920+CrimsonDevil333333@users.noreply.github.com>
2019-07-14 16:31:02 +0530

    Add files via upload

---
+++ Wikipedia With voice.py
@@ -0,0 +1,36 @@
+import wikipedia
+import pyttsx3
+p=print
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-5)
+flag=True
+engine.say("what ever you want to search in wikipedia you can ask me")
+p("what ever you want to search in wikipedia you can ask me")
+engine.runAndWait()
+while (flag==True):
+    engine.say('what do you want to know')
+    engine.runAndWait()
+    n=input("what do you want to know ---> ")
+    if(n=='quit' or n=='exit' or n=="QUIT" or n=="EXIT" or n=="Nothing" or n=="Quit" or n=="nothing" or n=="Exit"):
+        p("\nHope to see you Again\nThanks for giving us your precious time")
+        engine.say('Hope to see you Again')
+        engine.runAndWait()
+        engine.say("Thanks for giving us your precious time")
+        engine.runAndWait()
+        flag=False
+    else:
+        q=wikipedia.summary(n, sentences=3)
+        engine.say(q)
+        p("\n",q)
+        engine.runAndWait()
+        p("\n\n")
+        
+        
+    
+        
+    
+
+
+
+
